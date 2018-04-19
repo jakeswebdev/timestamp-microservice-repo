af65ec8c95d0888a253057c6660abf74385ca4da
jakeswebdev <jakeswebdev>
2018-04-19 16:05:30 -0500

    server.js

--- server.js
+++ server.js
@@ -4,31 +4,34 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 let app = express();
 
+let monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
 //returning the date values parsed in JSON
 app.get('/datevalues/:dateParam',function(request,response){
     let dateParam = request.params.dateParam;
     let momentDate = moment(dateParam);
-    let isItUnix = false;
     if(!isNaN(dateParam)){
-        isItUnix = true;
-        var unixToNormal = new Date(dateParam * 1000);
-        console.log(unixToNormal + "this is unix");
-        response.json({"unix":dateParam,"natural": unixToNormal});
+        let unixToNormal = new Date(dateParam * 1000);
+        let month = monthNames[unixToNormal.getMonth() +1];
+        let day = unixToNormal.getDate();
+        let year = unixToNormal.getFullYear();
+        let dateStr = month + " " + day + ", " + year;
+        console.log(dateStr);
+        response.json({"unix":Number(dateParam),"natural": dateStr });
     }
     if(momentDate.isValid() && isNaN(dateParam)){
-        
         let unixFromNormal = new Date(dateParam).getTime() / 1000;
         response.json({"unix":unixFromNormal,"natural":dateParam})
-        
     }
     else{
-        response.write("Please enter a valid date");
+        response.json({"unix":null,"natural":null});
     }
-    console.log("thi is the date parameter:" + dateParam);
-
-    response.send(console.log("this is the datevalues directory"));
 });
-
 //server listening on port 3000
 app.listen(3000,function(){
     console.log("server working");
